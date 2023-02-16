3f0e4138869dcc5f941bebfab821b9ac94a4fe26
Adel <aromat29.11@gmail.com>
2023-02-16 15:22:29 +0300

    popup implementation

--- WebPortfolioApp/urls.py
+++ WebPortfolioApp/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import ListProjects, ProjectDetail
+from .views import ListProjects, ProjectDetail, RegisterUser, LoginUser
 
 urlpatterns = [
     path('', ListProjects.as_view(), name='home'),
     path('project/<slug:proj_slug>/', ProjectDetail.as_view(), name='project'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('register/', RegisterUser.as_view(), name='register'),
 ]
 
 
--- WebPortfolioApp/views.py
+++ WebPortfolioApp/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
+from .forms import RegisterUserForm
 from .models import PortfolioStructure
 
 
@@ -18,3 +22,15 @@ class ProjectDetail(DetailView):
     context_object_name = 'project'
 
 
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'WebPortfolioApp/register_popup.html'
+    success_url = reverse_lazy('login')
+
+
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'WebPortfolioApp/login_popup.html'
+
+    def get_success_url(self):
+        return reverse_lazy('home')
